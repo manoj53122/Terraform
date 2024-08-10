ade45b2edcfc1696e3778282843538ad3fbfbfb0
manoj531222 <manoj.gandham404@gmail.com>
2024-08-10 18:45:42 +0530

    updated_file

--- VPCEndpoint.tf
+++ VPCEndpoint.tf
@@ -1,41 +1,11 @@
-# resource "aws_vpc_endpoint" "ssm" {
-#   count      = length(var.cidr_private_subnet)
-#   vpc_id = aws_vpc.main.id
-#   service_name = "com.amazonaws.us-east-1.ssm"
-#   subnet_ids = aws_subnet.private_subnets[*].id
-
-# }
-
-# resource "aws_vpc_endpoint" "ssm" {
-#   vpc_id             = aws_vpc.main.id
-#   service_name       = "com.amazonaws.us-east-1.ssm"
-#   vpc_endpoint_type  = "Interface" # This is important
-#   subnet_ids         = aws_subnet.private_subnets[*].id
-
-#   security_group_ids = ["aws_security_group.ec2_sg"] # Optional, depending on your requirements
-#   private_dns_enabled = true
-# }
-
-resource "aws_vpc_endpoint" "ssm" {
-  vpc_id             = aws_vpc.main.id
-  service_name       = "com.amazonaws.us-east-1.ssm"
-  vpc_endpoint_type  = "Interface"
-  subnet_ids         = aws_subnet.private_subnets[*].id
-
-  security_group_ids = [aws_security_group.ec2_sg.id]  # Reference the security group ID
-  private_dns_enabled = true
+resource "aws_vpc_attribute" "enable_dns_support" {
+  vpc_id     = aws_vpc.main.id
+  key        = "enableDnsSupport"
+  value      = true
 }
 
-
-
-# resource "aws_vpc_endpoint" "ec2messages" {
-#   vpc_id = aws_vpc.my_vpc.id
-#   service_name = "com.amazonaws.us-east-1.ec2messages"
-#   subnet_ids   = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
-# }
-
-# resource "aws_vpc_endpoint" "ssmmessages" {
-#   vpc_id = aws_vpc.my_vpc.id
-#   service_name = "com.amazonaws.us-east-1.ssmmessages"
-#   subnet_ids   = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
-# }
+resource "aws_vpc_attribute" "enable_dns_hostnames" {
+  vpc_id     = aws_vpc.main.id
+  key        = "enableDnsHostnames"
+  value      = true
+}
