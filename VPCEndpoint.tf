afecbd521bc5b418e958bf1cdadd7bbbc7817337
manoj531222 <manoj.gandham404@gmail.com>
2024-08-10 18:28:26 +0530

    updated_file

--- VPCEndpoint.tf
+++ VPCEndpoint.tf
@@ -4,14 +4,14 @@ resource "aws_vpc_endpoint" "ssm" {
   subnet_ids   = [aws_subnet.private_subnets_1.id, aws_subnet.private_subnet_2.id]
 }
 
-resource "aws_vpc_endpoint" "ec2messages" {
-  vpc_id = aws_vpc.my_vpc.id
-  service_name = "com.amazonaws.us-east-1.ec2messages"
-  subnet_ids   = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
-}
+# resource "aws_vpc_endpoint" "ec2messages" {
+#   vpc_id = aws_vpc.my_vpc.id
+#   service_name = "com.amazonaws.us-east-1.ec2messages"
+#   subnet_ids   = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+# }
 
-resource "aws_vpc_endpoint" "ssmmessages" {
-  vpc_id = aws_vpc.my_vpc.id
-  service_name = "com.amazonaws.us-east-1.ssmmessages"
-  subnet_ids   = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
-}
+# resource "aws_vpc_endpoint" "ssmmessages" {
+#   vpc_id = aws_vpc.my_vpc.id
+#   service_name = "com.amazonaws.us-east-1.ssmmessages"
+#   subnet_ids   = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+# }
