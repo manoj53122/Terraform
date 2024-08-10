0c67c397be5307362c703fced10a6a60cd038080
manoj531222 <manoj.gandham404@gmail.com>
2024-08-10 18:37:14 +0530

    updated_file

--- VPCEndpoint.tf
+++ VPCEndpoint.tf
@@ -1,10 +1,21 @@
-resource "aws_vpc_endpoint" "ssm" {
-  count      = length(var.cidr_private_subnet)
-  vpc_id = aws_vpc.main.id
-  service_name = "com.amazonaws.us-east-1.ssm"
-  subnet_ids = aws_subnet.private_subnets[*].id
+# resource "aws_vpc_endpoint" "ssm" {
+#   count      = length(var.cidr_private_subnet)
+#   vpc_id = aws_vpc.main.id
+#   service_name = "com.amazonaws.us-east-1.ssm"
+#   subnet_ids = aws_subnet.private_subnets[*].id
 
+# }
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id             = aws_vpc.main.id
+  service_name       = "com.amazonaws.us-east-1.ssm"
+  vpc_endpoint_type  = "Interface" # This is important
+  subnet_ids         = aws_subnet.private_subnets[*].id
+
+  security_group_ids = [aws_security_group.some_sg.id] # Optional, depending on your requirements
+  private_dns_enabled = true
 }
+
 
 # resource "aws_vpc_endpoint" "ec2messages" {
 #   vpc_id = aws_vpc.my_vpc.id
