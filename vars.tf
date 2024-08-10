7fbac40d5b25aea7246b976e48e0d44fec232397
manoj531222 <manoj.gandham404@gmail.com>
2024-08-10 19:17:20 +0530

    updated_file

--- vars.tf
+++ vars.tf
@@ -1,18 +1,18 @@
-# variable "vpc_cidr" {
-#   default     = "10.0.0.0/24"
-# }
+variable "vpc_cidr" {
+  default     = "10.0.0.0/24"
+}
 
-# variable "cidr_public_subnet" {
-#   description = "Public Subnet CIDR"
-#   default     = ["10.0.0.0/26", "10.0.0.64/26"]
-# }
+variable "cidr_public_subnet" {
+  description = "Public Subnet CIDR"
+  default     = ["10.0.0.0/26", "10.0.0.64/26"]
+}
 
-# variable "cidr_private_subnet" {
-#   description = "Private Subnet CIDR"
-#   default     = ["10.0.0.128/26", "10.0.0.192/26"]
-# }
+variable "cidr_private_subnet" {
+  description = "Private Subnet CIDR"
+  default     = ["10.0.0.128/26", "10.0.0.192/26"]
+}
 
-# variable "us-east_availability_zone" {
-#  description = "Availability Zones"
-#  default     = ["us-east-1a", "us-east-1b"]
-# }
+variable "us-east_availability_zone" {
+ description = "Availability Zones"
+ default     = ["us-east-1a", "us-east-1b"]
+}
