ed293a6230bd37de819b0ac1a5718872c4ef4090
manoj531222 <manoj.gandham404@gmail.com>
2024-08-10 15:00:19 +0530

    updated_file

--- NGW.tf
+++ NGW.tf
@@ -1,17 +1,13 @@
-# resource "aws_eip" "nat_eip" {
-#   count = length(var.cidr_private_subnet)
-# }
+resource "aws_eip" "nat_eip" {
+  count = length(var.cidr_private_subnet)
+}
 
-# resource "aws_nat_gateway" "nat_gateway" {
-#   count      = length(var.cidr_private_subnet)
-#   depends_on = [aws_eip.nat_eip]
-#   allocation_id = aws_eip.nat_eip[count.index].id
-#   subnet_id = module.aws_subnet.aws.private_subnets[count.index].id
-#   tags = {
-#     "Name" = "Private NAT GW: us-east-1 "
-#   }
-# }
-
-# output "private_subnet" {
-#   value = aws_subnet.private_subnets.id
-# }
+resource "aws_nat_gateway" "nat_gateway" {
+  count      = length(var.cidr_private_subnet)
+  depends_on = [aws_eip.nat_eip]
+  allocation_id = aws_eip.nat_eip[count.index].id
+  subnet_id = module.aws_subnet.aws.private_subnets[count.index].id
+  tags = {
+    "Name" = "Private NAT GW: us-east-1 "
+  }
+}
