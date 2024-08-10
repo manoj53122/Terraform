5be106f45fcbc0a3529daef347af52c6aff7ecdb
manoj531222 <manoj.gandham404@gmail.com>
2024-08-10 11:55:15 +0530

    updated_file

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
+  cidr_block = var.vpc_cidr
   enable_dns_support = true
   enable_dns_hostnames = true
   tags = {
     Name = "vpc : test-us-east-1"
+  }
+}
+resource "aws_subnet" "public_subnets" {
+  count      = length(var.cidr_public_subnet)
+  vpc_id     = aws_vpc.vpc_cidr
+  cidr_block = element(var.cidr_public_subnet, count.index)
+  availability_zone = element(var.us-east_availability_zone, count.index)
+
+  tags = {
+    Name = "Subnet-Public : Public Subnet ${count.index + 1}"
   }
 }
