bf5e9c416f5f1779846e066bb8c2aed18ad5cd40
manoj531222 <manoj.gandham404@gmail.com>
2024-08-10 18:32:40 +0530

    updated_file

--- VPCEndpoint.tf
+++ VPCEndpoint.tf
@@ -2,7 +2,7 @@ resource "aws_vpc_endpoint" "ssm" {
   count      = length(var.cidr_private_subnet)
   vpc_id = aws_vpc.main.id
   service_name = "com.amazonaws.us-east-1.ssm"
-  subnet_ids   = length("aws_subnet.private_subnets")
+  subnet_ids   = length["aws_subnet.private_subnets"]
 }
 
 # resource "aws_vpc_endpoint" "ec2messages" {
