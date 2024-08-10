77f1ecab2988277be60c5ec5ff8cb85136264095
manoj531222 <manoj.gandham404@gmail.com>
2024-08-10 18:58:11 +0530

    updated_file

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-output "id" {
-  value = aws_vpc.main.cidr_block
-}
+# output "id" {
+#   value = aws_vpc.main.cidr_block
+# }
 
-output "IGW" {
-  value = aws_internet_gateway.public_internet_gateway.id
-}
+# output "IGW" {
+#   value = aws_internet_gateway.public_internet_gateway.id
+# }
--- subnets.tf
+++ subnets.tf
@@ -1,21 +1,21 @@
-resource "aws_subnet" "public_subnets" {
-  count      = length(var.cidr_public_subnet)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.cidr_public_subnet, count.index)
-  availability_zone = element(var.us-east_availability_zone, count.index)
+# resource "aws_subnet" "public_subnets" {
+#   count      = length(var.cidr_public_subnet)
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = element(var.cidr_public_subnet, count.index)
+#   availability_zone = element(var.us-east_availability_zone, count.index)
 
-  tags = {
-    Name = "Subnet-Public : Public Subnet_${count.index + 1}"
-  }
-}
+#   tags = {
+#     Name = "Subnet-Public : Public Subnet_${count.index + 1}"
+#   }
+# }
 
-resource "aws_subnet" "private_subnets" {
-  count      = length(var.cidr_private_subnet)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.cidr_private_subnet, count.index)
-  availability_zone = element(var.us-east_availability_zone, count.index)
+# resource "aws_subnet" "private_subnets" {
+#   count      = length(var.cidr_private_subnet)
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = element(var.cidr_private_subnet, count.index)
+#   availability_zone = element(var.us-east_availability_zone, count.index)
 
-  tags = {
-    Name = "Subnet-Private : Private Subnet_${count.index + 1}"
-  }
-}
+#   tags = {
+#     Name = "Subnet-Private : Private Subnet_${count.index + 1}"
+#   }
+# }
