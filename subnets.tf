4378d30336ff6ba5fb8374082ed0e3259a3adf20
manoj531222 <manoj.gandham404@gmail.com>
2024-08-10 12:07:30 +0530

    updated_file

---
+++ subnets.tf
@@ -0,0 +1,21 @@
+resource "aws_subnet" "public_subnets" {
+  count      = length(var.cidr_public_subnet)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.cidr_public_subnet, count.index)
+  availability_zone = element(var.us-east_availability_zone, count.index)
+
+  tags = {
+    Name = "Subnet-Public : Public Subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count      = length(var.cidr_private_subnet)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.cidr_private_subnet, count.index)
+  availability_zone = element(var.us-east_availability_zone, count.index)
+
+  tags = {
+    Name = "Subnet-Private : Private Subnet ${count.index + 1}"
+  }
+}
