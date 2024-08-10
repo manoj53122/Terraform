16f118546d5a935d622635fad0a239a993433b5c
manoj531222 <manoj.gandham404@gmail.com>
2024-08-10 19:09:06 +0530

    updated_file

--- main.tf
+++ main.tf
@@ -29,13 +29,13 @@ resource "aws_subnet" "public_2" {
 # Create private subnets
 resource "aws_subnet" "private_1" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.10.10.128/26"
+  cidr_block = "10.10.10.128/27"
   availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "private_2" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.10.10.192/26"
+  cidr_block = "10.10.10.160/27"
   availability_zone = "us-east-1b"
 }
 
