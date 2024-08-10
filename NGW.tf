89ff03942e9ddae8177dfa098d8b5292226275cc
manoj531222 <manoj.gandham404@gmail.com>
2024-08-10 14:02:48 +0530

    updated_file

--- NGW.tf
+++ NGW.tf
@@ -6,7 +6,7 @@ resource "aws_nat_gateway" "nat_gateway" {
   count      = length(var.cidr_private_subnet)
   depends_on = [aws_eip.nat_eip]
   allocation_id = aws_eip.nat_eip[count.index].id
-  subnet_id = aws_subnet.private_subnet[count.index]
+  subnet_id = aws_subnet.private_subnet[count.index].subnet_id
   tags = {
     "Name" = "Private NAT GW: us-east-1 "
   }
