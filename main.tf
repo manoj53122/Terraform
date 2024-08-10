9683dd15b7a510a405624df8d5af637f06ff5367
manoj531222 <manoj.gandham404@gmail.com>
2024-08-10 19:13:01 +0530

    updated_file

--- main.tf
+++ main.tf
@@ -151,36 +151,36 @@ resource "aws_security_group" "alb_sg" {
   }
 }
 
-# Create VPC endpoints for SSM
-# Create VPC endpoints for SSM (Interface endpoints)
-resource "aws_vpc_endpoint" "ssm" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.us-east-1.ssm"
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = [aws_subnet.private_1.id, aws_subnet.private_2.id]
-  security_group_ids = [aws_security_group.ec2_sg.id]
+# # Create VPC endpoints for SSM
+# # Create VPC endpoints for SSM (Interface endpoints)
+# resource "aws_vpc_endpoint" "ssm" {
+#   vpc_id            = aws_vpc.main.id
+#   service_name      = "com.amazonaws.us-east-1.ssm"
+#   vpc_endpoint_type = "Interface"
+#   subnet_ids        = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+#   security_group_ids = [aws_security_group.ec2_sg.id]
 
-  private_dns_enabled = true
-}
+#   private_dns_enabled = true
+# }
 
-resource "aws_vpc_endpoint" "ssm_messages" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.us-east-1.ssmmessages"
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = [aws_subnet.private_1.id, aws_subnet.private_2.id]
-  security_group_ids = [aws_security_group.ec2_sg.id]
+# resource "aws_vpc_endpoint" "ssm_messages" {
+#   vpc_id            = aws_vpc.main.id
+#   service_name      = "com.amazonaws.us-east-1.ssmmessages"
+#   vpc_endpoint_type = "Interface"
+#   subnet_ids        = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+#   security_group_ids = [aws_security_group.ec2_sg.id]
 
-  private_dns_enabled = true
-}
+#   private_dns_enabled = true
+# }
 
-resource "aws_vpc_endpoint" "ec2_messages" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.us-east-1.ec2messages"
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = [aws_subnet.private_1.id, aws_subnet.private_2.id]
-  security_group_ids = [aws_security_group.ec2_sg.id]
+# resource "aws_vpc_endpoint" "ec2_messages" {
+#   vpc_id            = aws_vpc.main.id
+#   service_name      = "com.amazonaws.us-east-1.ec2messages"
+#   vpc_endpoint_type = "Interface"
+#   subnet_ids        = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+#   security_group_ids = [aws_security_group.ec2_sg.id]
 
-  private_dns_enabled = true
+#   private_dns_enabled = true
 }
 
 
