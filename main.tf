aa23717230aa6bc5b5f0748dc0ceb57ccc4ca770
manoj531222 <manoj.gandham404@gmail.com>
2024-08-10 19:38:25 +0530

    updated_file

--- main.tf
+++ main.tf
@@ -157,14 +157,6 @@ resource "aws_vpc_endpoint" "ssm" {
 }
 
 # Your existing VPC Endpoint resources
-resource "aws_vpc_endpoint" "ssm" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.us-east-1.ssm"
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = [aws_subnet.private_1.id, aws_subnet.private_2.id]
-  security_group_ids = [aws_security_group.ec2_sg.id]
-  private_dns_enabled = true
-}
 
 resource "aws_vpc_endpoint" "ssm_messages" {
   vpc_id            = aws_vpc.main.id
