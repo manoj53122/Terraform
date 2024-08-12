6cb56b61aac69a52dfe7ba983612a30c44a17c10
manoj531222 <manoj.gandham404@gmail.com>
2024-08-12 15:42:46 +0530

    updated_file

--- subnets.tf
+++ subnets.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "private_instance" {
-  ami           = "ami-04a81a99f5ec58529"  # Replace with the appropriate AMI ID
+  ami           = "ami-04a81a99f5ec58529"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.private_subnet_1.id
+  subnet_id     = aws_subnet.private_1.id
   security_groups = [aws_security_group.ec2_sg.name]
   associate_public_ip_address = false
   user_data = <<-EOF
